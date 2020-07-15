eacc48ebb76a56c5b386c0317a40cabd406274a4
rizwan <iamrizwan40@gmail.com>
2020-07-15 08:04:13 -0700

    some style chnages on 15/07

--- views/js/main.js
+++ views/js/main.js
@@ -1,18 +1,13 @@
+
 document.addEventListener('DOMContentLoaded', function(){
     const hamburgerIcon = document.querySelector('div.nav-left-menus span.hamburger-icon');    
-    document.querySelectorAll('#article').forEach(article => {
-        article.addEventListener('click', (e) => {
-            console.log(e.target.id)
-        })
-    })
 
     hamburgerIcon.addEventListener('click', function(e){
         e.preventDefault()
         document.querySelector('div#sidenav-wrapper').classList.add('sidenav-wrapper-toggle')
         document.querySelector('nav.sidenav').classList.add('sidenav-toggle');
         document.querySelector('body').classList.add("overflow-hidden");
-        document.querySelector('body').style.transition= "400ms ease-in-out";
-        console.log(e.target)
+        document.querySelector('body').style.transition= "400ms ease-in-out";        
     })
     
     document.querySelector('nav.sidenav div.header span.close').addEventListener('click', (e) => {
@@ -26,9 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let trueOrFalse = false;
     caret.addEventListener('click', (e) => {        
         e.preventDefault();       
-        trueOrFalse = !trueOrFalse
-        console.log(trueOrFalse)
-        
+        trueOrFalse = !trueOrFalse                
         if(trueOrFalse){
             document.querySelector('ul.user-menus li ul').classList.add('toggle-user-menus')
             document.querySelector('div#dropdown-wrapper').classList.add('toggle-dropdown')
@@ -37,6 +30,41 @@ document.addEventListener('DOMContentLoaded', function(){
             document.querySelector('div#dropdown-wrapper').classList.remove('toggle-dropdown')
         }
     }) 
+    const underDev = document.querySelector('div.under-development-inform');
+    const label = document.querySelector('div.under-development-inform div.label');
+    const closeLabel = document.querySelector('div.under-development-inform div.label div.contents a');          
+    let title = document.querySelector('title').innerHTML;
+    if(title == 'CodAcad'){
+        underDev.classList.add('show-under-development')    
+        label.classList.add('show-label')    
+        closeLabel.addEventListener('click', (e) => {
+            e.preventDefault()
+            underDev.classList.remove('show-under-development')
+            label.classList.remove('show-label')
+        })    
+
+        const devContents = document.querySelector('div.developer-contents');
+        const developer = devContents.querySelector('div#developer-one');
+        const devContentsCords = devContents.getBoundingClientRect();
+        const developersCords = developer.getBoundingClientRect();
+        console.log(devContentsCords);
+        devContents.onscroll = function(e){
+            
+        }
+    }    
+
+    if(title == "Resume"){
+        window.onscroll = function(e){        
+            const resume = document.getElementById('resume');
+            const resumeCords = resume.getBoundingClientRect();
+            const navbar = document.querySelector("nav#navbar");        
+            if(resumeCords.y <= -10){
+                navbar.style.boxShadow = "0px 1px 10px rgba(0,0,0,.5)";
+            }else if(resumeCords.y == 55){
+                navbar.style.boxShadow = "none";
+            }
+        }
+    }   
     window.onclick = (e) => {
         if(e.target.id == "sidenav-wrapper"){
             document.querySelector('div#sidenav-wrapper').classList.remove('sidenav-wrapper-toggle');
@@ -50,56 +78,39 @@ document.addEventListener('DOMContentLoaded', function(){
          if(e.target.id == 'under-development'){
             underDev.classList.remove('show-under-development')
             label.classList.remove('show-label')
-        }       
-        document.querySelectorAll('div.frequency span.bar').forEach(function(progress){
-            const span = document.createElement('span');
-            span.className = 'progress';
-            progress.append(span);
-            const percentage = progress.nextElementSibling.innerHTML;
-            progress.querySelector('span.progress').style.width = `${percentage}`       ;
-            
-        })            
-        document.querySelector('div.page1').style.display = "block";
-        function showPage(page){
-            document.querySelectorAll('div.page').forEach(div => {
-                div.style.display = 'none'
-            })                
-            document.querySelector(`#${page}`).style.display = 'block';            
-    
-        }    
-        function addActiveClass(button){
-            document.querySelectorAll('a.btn').forEach(btn => {
-                btn.classList.remove('active');
-            })
-    
-            document.querySelector(`#${button}`).classList.add('active');
-            console.log(button)
-    
-        }
-        const primaryTitle = document.querySelector('title').innerHTML
-        document.querySelector('title').innerHTML = `${primaryTitle} (${document.querySelector('a.btn').innerHTML})`
-        document.querySelectorAll('a.btn').forEach(function(btn){        
-            btn.onclick = function(){                                     
-                showPage(this.dataset.page);                              
-                document.querySelector('title').innerHTML = `${primaryTitle} (${this.innerHTML})`                                                               
-                addActiveClass(this.id);
-            }                  
-        })     
+        }              
     }
-    const underDev = document.querySelector('div.under-development-inform');
-    const label = document.querySelector('div.under-development-inform div.label');
-    const closeLabel = document.querySelector('div.under-development-inform div.label div.contents a');          
-    let title = document.querySelector('title').innerHTML;
-    if(title == 'CodAcad'){
-        underDev.classList.add('show-under-development')    
-        label.classList.add('show-label')    
-        closeLabel.addEventListener('click', (e) => {
-            e.preventDefault()
-            underDev.classList.remove('show-under-development')
-            label.classList.remove('show-label')
-        })    
-    }    
+    document.querySelectorAll('div.frequency span.bar').forEach(function(progress){
+        const span = document.createElement('span');
+        span.className = 'progress';
+        progress.append(span);
+        const percentage = progress.nextElementSibling.innerHTML;
+        progress.querySelector('span.progress').style.width = `${percentage}`       ;
+        
+    })            
+    document.querySelector('div.page1').style.display = "block";
+    function showPage(page){
+        document.querySelectorAll('div.page').forEach(div => {
+            div.style.display = 'none'
+        })                
+        document.querySelector(`#${page}`).style.display = 'block';            
 
+    }    
+    function addActiveClass(button){
+        document.querySelectorAll('a.btn').forEach(btn => {
+            btn.classList.remove('active');
+        })
+        document.querySelector(`#${button}`).classList.add('active');        
+    }
+    const primaryTitle = document.querySelector('title').innerHTML
+    document.querySelector('title').innerHTML = `${primaryTitle} (${document.querySelector('a.btn').innerHTML})`
+    document.querySelectorAll('a.btn').forEach(function(btn){        
+        btn.onclick = function(){                                     
+            showPage(this.dataset.page);                              
+            document.querySelector('title').innerHTML = `${primaryTitle} (${this.innerHTML})`                                                               
+            addActiveClass(this.id);
+        }                  
+    })            
 })
              
     
