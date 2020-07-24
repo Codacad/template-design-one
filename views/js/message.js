b6e7016c563d76a1e68090e02471aa0b271d2998
rizwan <iamrizwan40@gmail.com>
2020-07-24 00:46:24 -0700

    added dynamic post route

--- views/js/message.js
+++ views/js/message.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('#form');    
-    form.addEventListener('submit', sendMessage);                
+    form.addEventListener('submit', sendMessage);                    
 })
 function sendMessage(e) {    
     e.preventDefault();        
-    let URL = "http://www.codacad.com/message";
+    let URL;
+    if(location.hostname == "localhost"){
+        URL = "http://localhost:5000/message";        
+    }else{
+        URL = "http://www.codacad.com/message";
+    }
+    console.log(URL)
+
     let localURL = "http://localhost:5000/message";
     let nameInput = document.querySelector('#name')
     let emailInput = document.querySelector('#email')
@@ -19,7 +26,10 @@ function sendMessage(e) {
         text            
     }    
     if(!nameInput.value || !emailInput.value || !textInput.value){
-        console.log('Please fill all fields')
+        document.querySelector('form div.success').classList.add('show-fail')        
+        let fail = document.querySelector('form div.success.show-fail');
+        fail.querySelector('span').textContent = "Please fill all fields..."
+
     }else{
         fetch(localURL, {
             method:'POST',            
@@ -35,6 +45,8 @@ function sendMessage(e) {
             console.log(error)
         });
         document.querySelector('form div.success').classList.add('show-success')        
+        let success = document.querySelector('form div.success.show-success');
+        success.querySelector('span').textContent = "Message sent successfully..."
         nameInput.value = "";
         emailInput.value= ""
         textInput.value = ""
@@ -42,6 +54,11 @@ function sendMessage(e) {
     if(document.querySelector('form div.success').classList.contains("show-success")){
         setTimeout(() => {
             document.querySelector('form div.success').classList.remove('show-success');
+        }, 5000)
+    }
+    if(document.querySelector('form div.success').classList.contains("show-fail")){
+        setTimeout(() => {
+            document.querySelector('form div.success').classList.remove('show-fail');
         }, 5000)
     }
 }
